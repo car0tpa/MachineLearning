5b1d4a254cee4c500c72933bac0210c6e2863915
XWHQSJ <xwhqsj@gmail.com>
2019-03-06 22:08:32 +0800

    add MLPractice/100-Days-Of-ML-Code/Day43_44_K_Means_Clustering

--- MLPractice/Deep Learning Based on Python/Deep Learning Based on Python Code/ch04/two_layer_net.py
+++ MLPractice/Deep Learning Based on Python/Deep Learning Based on Python Code/ch04/two_layer_net.py
@@ -9,11 +9,8 @@ class TwoLayerNet:
 
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
         # 初始化权重
-        self.params = {}
-        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
-        self.params['b1'] = np.zeros(hidden_size)
-        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
-        self.params['b2'] = np.zeros(output_size)
+        self.params = {'W1': weight_init_std * np.random.randn(input_size, hidden_size), 'b1': np.zeros(hidden_size),
+                       'W2': weight_init_std * np.random.randn(hidden_size, output_size), 'b2': np.zeros(output_size)}
 
     def predict(self, x):
         W1, W2 = self.params['W1'], self.params['W2']
